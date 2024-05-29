7e529839409cc218abed080c91d6fdf792124e8a
Chandana2510 <chandanagovada12@gmail.com>
2024-05-29 16:26:09 +0530

    adding env

--- src/App.tsx
+++ src/App.tsx
@@ -47,8 +47,8 @@ function App() {
   const [organizationID, setOrganizationID] = useState(() => {
     return localStorage.getItem("organizationID") || "";
   });
-  const sessionTimeout = process.env.VITE_APP_SESSION;
-  console.log(`Session Timeout: ${sessionTimeout}`); // This line checks the value
+  const session_time = import.meta.env.VITE_APP_SESSION;
+  console.log(`Session Timeout: ${session_time}`); 
 
   useOrganizationRedirect(organizationID, navigate);
   useLanguageChange(language, i18n);

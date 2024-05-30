3d250ec7400faf605ccfc57684135870fe096dfc
Chandana2510 <chandanagovada12@gmail.com>
2024-05-30 15:11:00 +0530

    adding the test case

--- src/api/const/sessionTimeOut.ts
+++ src/api/const/sessionTimeOut.ts
@@ -1,9 +1,9 @@
 const session_time = {
-  VITE_APP_SESSION: parseInt(import.meta.env.VITE_APP_SESSION)
+  VITE_APP_SESSION: parseInt(process.env.VITE_APP_SESSION)
 };
 
 const ApiUrl = {
-  VITE_APP_BASE_URL: import.meta.env.VITE_APP_BASE_URL // This is expected to be a string
+  VITE_APP_BASE_URL: process.env.VITE_APP_BASE_URL // This is expected to be a string
 };
 
 const config = { session_time, ApiUrl };

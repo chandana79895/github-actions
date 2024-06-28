2ebddcf06610ed990999b26acc85b09f5fc14588
Chandana2510 <chandanagovada12@gmail.com>
2024-06-28 15:14:01 +0530

    commented step in pipeline

--- automation-testing/src/main/java/zapcg/Capillary/Base/BaseTest.java
+++ automation-testing/src/main/java/zapcg/Capillary/Base/BaseTest.java
@@ -3,7 +3,6 @@ package zapcg.Capillary.Base;
 import java.time.Duration;
 
 import org.openqa.selenium.Dimension;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,12 +13,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import zapcg.Cappilary.utils.ConfigReader;
 import zapcg.Cappilary.utils.DeviceData;
 
-
 public class BaseTest {
-	public static WebDriver driver;
+    public static WebDriver driver;
     protected String baseUrl;
     protected Dimension dimension;
-    protected String deviceName; 
+    protected String deviceName;
 
     public void setUp(String browser, String deviceName) {
         ConfigReader configReader = new ConfigReader();
@@ -29,36 +27,32 @@ public class BaseTest {
     }
 
     public void initialization(String browser) {
-    	try {
-        if (browser.equalsIgnoreCase("chrome")) {
-        	WebDriverManager.chromedriver().setup();
-        	 ChromeOptions options = new ChromeOptions();
-             options.addArguments("--remote-allow-origins=*");
-          // Adding detach option
-             options.setExperimentalOption("detach", true);
-          driver = new ChromeDriver(options);
-        
-            
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        }
-
-        if (dimension != null) {
-            driver.manage().window().setSize(dimension);    
+        try {
+            if (browser.equalsIgnoreCase("chrome")) {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                // Adding detach option
+                options.setExperimentalOption("detach", true);
+                driver = new ChromeDriver(options);
+            } else if (browser.equalsIgnoreCase("firefox")) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
             }
-    	driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-    } catch (Exception e) {
-        System.err.println("Error initializing WebDriver: " + e.getMessage());
-        if (driver != null) {
-            driver.quit();
+
+            if (dimension != null) {
+                driver.manage().window().setSize(dimension);
+            }
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        } catch (Exception e) {
+            System.err.println("Error initializing WebDriver: " + e.getMessage());
+            if (driver != null) {
+                driver.quit();
+            }
         }
     }
-    
-}
-    
-    
+
     @AfterMethod
     public void tearDown() {
         try {
@@ -71,15 +65,11 @@ public class BaseTest {
         } finally {
             if (driver != null) {
                 try {
-            	driver.quit();
-                }catch (Exception e) {
+                    driver.quit();
+                } catch (Exception e) {
                     System.err.println("Error closing WebDriver session: " + e.getMessage());
                 }
-                
             }
         }
     }
-    
-
-
 }

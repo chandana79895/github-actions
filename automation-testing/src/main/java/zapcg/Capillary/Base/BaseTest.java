91eec5a8a56639d3695ccabb08cd6745dee751b8
Chandana2510 <chandanagovada12@gmail.com>
2024-06-28 15:52:22 +0530

    updated

--- automation-testing/src/main/java/zapcg/Capillary/Base/BaseTest.java
+++ automation-testing/src/main/java/zapcg/Capillary/Base/BaseTest.java
@@ -3,21 +3,23 @@ package zapcg.Capillary.Base;
 import java.time.Duration;
 
 import org.openqa.selenium.Dimension;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import zapcg.Cappilary.utils.ConfigReader;
 import zapcg.Cappilary.utils.DeviceData;
 
+
 public class BaseTest {
-    public static WebDriver driver;
+	public static WebDriver driver;
     protected String baseUrl;
     protected Dimension dimension;
-    protected String deviceName;
+    protected String deviceName; 
 
     public void setUp(String browser, String deviceName) {
         ConfigReader configReader = new ConfigReader();
@@ -27,33 +29,36 @@ public class BaseTest {
     }
 
     public void initialization(String browser) {
-        try {
-            if (browser.equalsIgnoreCase("chrome")) {
-                // Set up WebDriverManager for Chrome
-                WebDriverManager.chromedriver().version("93.0.4577.15").setup();
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--remote-allow-origins=*");
-                options.setExperimentalOption("detach", true);
-                driver = new ChromeDriver(options);
-            } else if (browser.equalsIgnoreCase("firefox")) {
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-            }
+    	try {
+        if (browser.equalsIgnoreCase("chrome")) {
+        	WebDriverManager.chromedriver().setup();
+        	 ChromeOptions options = new ChromeOptions();
+             options.addArguments("--remote-allow-origins=*");
+          // Adding detach option
+             options.setExperimentalOption("detach", true);
+          driver = new ChromeDriver(options);
+        
+            
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
 
-
-            if (dimension != null) {
-                driver.manage().window().setSize(dimension);
+        if (dimension != null) {
+            driver.manage().window().setSize(dimension);    
             }
-            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        } catch (Exception e) {
-            System.err.println("Error initializing WebDriver: " + e.getMessage());
-            if (driver != null) {
-                driver.quit();
-            }
+    	driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    } catch (Exception e) {
+        System.err.println("Error initializing WebDriver: " + e.getMessage());
+        if (driver != null) {
+            driver.quit();
         }
     }
-
+    
+}
+    
+    
     @AfterMethod
     public void tearDown() {
         try {
@@ -66,11 +71,15 @@ public class BaseTest {
         } finally {
             if (driver != null) {
                 try {
-                    driver.quit();
-                } catch (Exception e) {
+            	driver.quit();
+                }catch (Exception e) {
                     System.err.println("Error closing WebDriver session: " + e.getMessage());
                 }
+                
             }
         }
     }
+    
+
+
 }

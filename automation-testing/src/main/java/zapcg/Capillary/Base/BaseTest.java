5d288fc4d382ff71a113b046cb051f865df7d4be
Chandana2510 <chandanagovada12@gmail.com>
2024-07-16 13:37:53 +0530

    driver check

--- automation-testing/src/main/java/zapcg/Capillary/Base/BaseTest.java
+++ automation-testing/src/main/java/zapcg/Capillary/Base/BaseTest.java
@@ -1,29 +1,23 @@
 package zapcg.Capillary.Base;
 
-import java.io.File;
-import java.io.IOException;
 import java.time.Duration;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import zapcg.Cappilary.utils.ConfigReader;
 import zapcg.Cappilary.utils.DeviceData;
 
 public class BaseTest {
     public static WebDriver driver;
-    public String baseUrl;
-    public Dimension dimension;
-    public String deviceName; 
+    protected String baseUrl;
+    protected Dimension dimension;
+    protected String deviceName;
 
     public void setUp(String browser, String deviceName) {
         ConfigReader configReader = new ConfigReader();
@@ -35,26 +29,41 @@ public class BaseTest {
     public void initialization(String browser) {
         try {
             if (browser.equalsIgnoreCase("chrome")) {
-                WebDriverManager.chromedriver().setup();
+                // WebDriverManager.chromedriver().setup(); // Use WebDriverManager for managing ChromeDriver versions dynamically
+                System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--headless"); // Enable headless mode
+                options.addArguments("--no-sandbox");
+                options.setExperimentalOption("detach", true);
+                options.setAcceptInsecureCerts(true);
+
                 // Adding detach option
                 options.setExperimentalOption("detach", true);
+                // Handling SSL certificates
+                options.setAcceptInsecureCerts(true);
+
+                // Initialize ChromeDriver
                 driver = new ChromeDriver(options);
             } else if (browser.equalsIgnoreCase("firefox")) {
-                WebDriverManager.firefoxdriver().setup();
+                // WebDriverManager.firefoxdriver().setup(); // Use WebDriverManager for managing GeckoDriver versions dynamically
+                System.setProperty("webdriver.gecko.driver", "C:\\BrowserDriver\\FirefoxDriver\\geckodriver.exe"); // Adjust path as necessary
                 FirefoxOptions options = new FirefoxOptions();
+                // options.addArguments("--headless"); // Enable headless mode
+
+                // Handling SSL certificates
+                options.setAcceptInsecureCerts(true);
+
+                // Initialize FirefoxDriver
                 driver = new FirefoxDriver(options);
-            } else {
-                throw new IllegalArgumentException("Browser " + browser + " not supported");
             }
 
             if (dimension != null) {
-                driver.manage().window().setSize(dimension);    
+                driver.manage().window().setSize(dimension);
             }
+
             driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-            driver.get(baseUrl);
         } catch (Exception e) {
             System.err.println("Error initializing WebDriver: " + e.getMessage());
             if (driver != null) {
@@ -81,14 +90,5 @@ public class BaseTest {
                 }
             }
         }
-    }
-
-    public String getScreenShotPath(String TestCaseName, WebDriver driver) throws IOException {
-        TakesScreenshot ts = (TakesScreenshot) driver;
-        File source = ts.getScreenshotAs(OutputType.FILE);
-        String destPath = System.getProperty("user.dir") + "\\reports\\FailedTest" + TestCaseName + ".png";
-        File file = new File(destPath);
-        FileUtils.copyFile(source, file);
-        return destPath;
     }
 }

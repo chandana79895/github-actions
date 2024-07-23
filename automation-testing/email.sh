f370d85bdc84eff5e22f3f2f88ca9baccc647672
Chandana2510 <chandanagovada12@gmail.com>
2024-07-23 16:44:27 +0530

    check

--- automation-testing/email.sh
+++ automation-testing/email.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# Define variables
-EMAIL_TO="manichandana7989@gmail.com"
-EMAIL_SUBJECT="Automated Report"
-EMAIL_BODY="Please find the attached report."
-REPORT_FILE="test-output/emailable-report.html"
+# Set the recipient email address
+TO="manichandana7989@gmail.com"
 
-# Send the email using mutt
-echo "$EMAIL_BODY" | mutt -s "$EMAIL_SUBJECT" -a "$REPORT_FILE" -- "$EMAIL_TO"
+# Set the subject of the email
+SUBJECT="Automated Test Report"
+
+# Set the body of the email
+BODY="Please find the attached emailable report for the recent test run."
+
+# Path to the report file
+ATTACHMENT="test-output/emailable-report.html"
+
+# Send the email with the attachment
+mail -s "$SUBJECT" -a "$ATTACHMENT" "$TO" <<< "$BODY"
+
+echo "Email sent successfully."

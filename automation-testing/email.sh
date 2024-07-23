dcab2e980f49d08c36badd1f4bab86e693badfcb
Chandana2510 <chandanagovada12@gmail.com>
2024-07-23 16:36:32 +0530

    check email

--- automation-testing/email.sh
+++ automation-testing/email.sh
@@ -6,5 +6,5 @@ EMAIL_SUBJECT="Automated Report"
 EMAIL_BODY="Please find the attached report."
 REPORT_FILE="test-output/emailable-report.html"
 
-# Send the email
-echo "$EMAIL_BODY" | mail -s "$EMAIL_SUBJECT" -a "$REPORT_FILE" "$EMAIL_TO"
+# Send the email using mutt
+echo "$EMAIL_BODY" | mutt -s "$EMAIL_SUBJECT" -a "$REPORT_FILE" -- "$EMAIL_TO"
